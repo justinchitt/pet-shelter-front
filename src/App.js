f34497df9dbd9d8bb93378678e6f1c630eef5bad
Justin Chittarath <justinchitt@gmail.com>
2021-11-15 17:47:56 -0700

    Finished making nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Header from "./Header"
+import Navbar from "./Navbar"
+import Animals from "./Animals";
+import Applications from "./Applications";
+import Shelters from "./Shelters";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-
+      <Header/>
+      <BrowserRouter>
+      <Navbar/>
+        <Switch>
+          <Route path="/animals">
+            <Animals />
+          </Route>
+          <Route path="/applications">
+            <Applications />
+          </Route>
+          <Route path="/shelters">
+            <Shelters />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Applications.js
@@ -0,0 +1,7 @@
+function Applications() {
+    return (
+        <h1>Applications</h1>
+    )
+}
+
+export default Applications
