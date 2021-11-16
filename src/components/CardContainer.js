3d32144353dbb9beb083c3d0ac5ad38d79bde191
Katelyn <katelynjewel@gmail.com>
2021-11-15 16:21:42 -0800

    started card components

---
+++ src/components/CardContainer.js
@@ -0,0 +1,16 @@
+import CardView from "./CardView";
+
+function AllCards() {
+
+    return(
+        <div>
+            //search goes here
+            //form goes here
+            <div className="cards">
+                <CardView/>
+            </div>
+        </div>
+    )
+}
+
+export default AllCards;
