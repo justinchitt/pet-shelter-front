2799f22a43973ff13320182015c789b16641ee69
Lisa <redlisa125@gmail.com>
2021-11-16 08:26:30 -1000

    started delete button

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -2,12 +2,17 @@ import CardView from "./CardView";
 
 function AllCards() {
 
+    function handleDeleteItem(deletedItem) {
+        const updatedItems = ItemList.filter((deletedItem) => item.id !== deletedItem.id);
+          setItemList(updatedItems);
+        }
+
     return(
         <div>
             //search goes here
             //form goes here
             <div className="cards">
-                <CardView/>
+                <CardView handleDeleteItem={handleDeleteItem}/>
             </div>
         </div>
     )
