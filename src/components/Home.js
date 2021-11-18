a6ea4271032cd89f49a26a91a0184022c1184d06
Lisa <redlisa125@gmail.com>
2021-11-18 08:14:43 -1000

    started css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,21 @@
+import styled from 'styled-components'
+
 function Home() {
     return (
-        <h1>Home</h1>
+        <HomePage>
+            <h1>Best Friend Finder</h1>
+            <h2>A Shelter Resource</h2>
+        </HomePage>
+        
     )
 }
 
 export default Home
+
+
+const HomePage = styled.body`
+    // height: 100%;
+    // width: 100%;
+    background-image: url(src/images/doggy_friend.jpg);
+    background-size: cover;
+`
