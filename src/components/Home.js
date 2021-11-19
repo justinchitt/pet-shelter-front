91af2354327bfb3b1413a08120790a6d67638e68
Lisa <redlisa125@gmail.com>
2021-11-18 14:02:55 -1000

    fixed form displays

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,10 @@
-import styled from 'styled-components'
-
 function Home() {
     return (
-        <HomePage>
+        <div className="home">
             <h1>Best Friend Finder</h1>
             <h2>A Shelter Resource</h2>
-        </HomePage>
-        
+        </div>
     )
 }
 
 export default Home
-
-
-const HomePage = styled.body`
-    // height: 100%;
-    // width: 100%;
-    background-image: url(src/images/doggy_friend.jpg);
-    background-size: cover;
-`
